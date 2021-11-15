943218099013115acd48783e784b9f79a1227a0e
shruti07 <shruti.reshu@gmail.com>
2021-11-15 22:08:01 +0530

    Add files via upload

---
+++ challenge3/challenge3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenge3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //pass nested dictionary and key to a method and get output
+            var inputdict = new Dictionary<string, Dictionary<string, Dictionary<string, string>>>()
+            {
+                {
+                    "Shr",
+                        new Dictionary<string, Dictionary<string, string>>
+                        {
+                            {
+                                "Professionalname",
+                                    new Dictionary<string, string>
+                                    {
+                                        {"Firstname", "Shruti"}
+                                    }
+                            }
+                        }
+                },
+                {
+                    "Shashank",
+                        new Dictionary<string, Dictionary<string, string>>
+                        {
+                            {
+                                "Professionalname",
+                                    new Dictionary<string, string>
+                                    {
+                                        {"Firstname", "Shashank"}
+                                    }
+                            }                            
+                        }
+                }
+            };
+
+            var result = GetValueFromKey(inputdict,"Shashank");
+            foreach (var pair in result)
+            {
+                foreach (var innerPair in pair.Value)
+                {
+                    Console.WriteLine("{0} : {1} : {2}", pair.Key, innerPair.Key, innerPair.Value);
+                    Console.ReadLine();
+                }
+            }
+
+        }
+
+        public static Dictionary<string, Dictionary<string, string>> GetValueFromKey(Dictionary<string, Dictionary<string, Dictionary<string, string>>> dict, string inputkey)
+        {
+            Dictionary<string, Dictionary<string, string>> outputVal = new Dictionary<string, Dictionary<string, string>>();
+            if (dict.ContainsKey(inputkey))
+            {
+                outputVal = dict[inputkey];
+            }
+
+            return outputVal;
+        }
+    }
+}
