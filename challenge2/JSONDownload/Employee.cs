3be2e11a07424a30f85b5a8a900cc75117ead6c1
shruti07 <shruti.reshu@gmail.com>
2021-11-15 21:59:56 +0530

    Add files via upload

---
+++ challenge2/JSONDownload/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONDownload
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string country { get; set; }
+        public string gender { get; set; }
+        public string creditBalance { get; set; }
+    }
+}
---
+++ challenge2/JSONDownload/Program.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Azure;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JSONDownload
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = CloudConfigurationManager.GetSetting("BlobConnectionString");
+            BlobContainerClient container = new BlobContainerClient(connectionString, "challenge2blob");
+            var blockBlob = container.GetBlobClient("download/EmployeeData.json");
+            using (var fileStream = System.IO.File.OpenWrite(@"C:\Users\sagarwa5\Downloads\Output\result.json"))
+            {
+                blockBlob.DownloadTo(fileStream);
+            }
+
+            List<Employee> ro = new List<Employee>();
+            using (StreamReader r = new StreamReader(@"C:\Users\sagarwa5\Downloads\Output\result.json"))
+            {
+                string json = r.ReadToEnd();
+                Console.Write(json);
+                Console.ReadLine();
+                //ro = JsonConvert.DeserializeObject<List<Employee>>(json);
+            }
+
+        }
+    }
+}
